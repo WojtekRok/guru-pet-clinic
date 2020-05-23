631d28d5ff89f486a7fc1fce2ee6bbead9bda6a1
WojtekRok <57679791+WojtekRok@users.noreply.github.com>
2020-05-23 11:32:05 +0200

    Initial Project Add

---
+++ src/test/java/pl/wojtekrok/gurupetclinic/GuruPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wojtekrok.gurupetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuruPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

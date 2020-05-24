b637d840e717d8e506ef32309943c84aa877bdc4
WojtekRok <57679791+WojtekRok@users.noreply.github.com>
2020-05-24 18:37:45 +0200

    adding Controller and Index, adding MapServices and CrudService

--- pet-clinic-data/src/main/java/pl/wojtekrok/gurupetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/pl/wojtekrok/gurupetclinic/services/PetService.java
@@ -2,13 +2,6 @@ package pl.wojtekrok.gurupetclinic.services;
 
 import pl.wojtekrok.gurupetclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long>{
 
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save (Pet pet);
-
-    Set<Pet> findAll();
 }
